b305d6206d19dfd9f9a067c9692a4fe729f265ab
doge98 <luisvalve_197@hotmail.com>
2020-05-18 02:17:38 -0500

    asd
    asd

--- ws_tesis/ws/web/repositorio.DNIbuscar.php
+++ ws_tesis/ws/web/repositorio.DNIbuscar.php
@@ -1,12 +1,8 @@
 <?php
-
     $dni = $_POST["dni"];
     $fechaNac = $_POST["fechaNac"];
     $sexo = $_POST["sexo"];
-
     $json = file_get_contents('http://app20.susalud.gob.pe:8080/registro-renipress-webapp/login.htm?action=buscarPersona&dat_fechaNacimiento='.$fechaNac.'&cmb_sexo='.$sexo.'&cmb_tipoDocumentoIdentidad=1&txt_numeroDocumentoIdentidad='.$dni.'');
-
     $data = json_decode($json, true);
     echo json_encode($data);
-
 ?>
