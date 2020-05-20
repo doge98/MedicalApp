a3675b1b211209b2318cab9a0389402d69cc1b94
doge98 <doge98@github.com>
2020-05-20 15:21:15 -0500

    Update configuracion.php

--- ws_tesis/datos/configuracion.php
+++ ws_tesis/datos/configuracion.php
@@ -5,7 +5,7 @@
 define("SERVIDOR_BD", "tesis.c6p75ldcjydd.sa-east-1.rds.amazonaws.com");
 //define("SERVIDOR_BD", "34.67.39.231");
 define("USUARIO_BD", "postgres");
-define("CLAVE_BD", "Luisvalve123");
+define("CLAVE_BD", "luisvalve123");
 define("PUERTO_BD", "5432");
 define("NOMBRE_BD", "tesis");
 error_reporting(E_ALL ^ E_NOTICE);
