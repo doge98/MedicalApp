641a1784d4a65de6c4f722055fed8ee93af5782d
doge98 <doge98@github.com>
2020-05-22 16:04:53 -0500

    Update ipressmedico.registrar.php

--- ws_tesis/ws/web/ipressmedico.registrar.php
+++ ws_tesis/ws/web/ipressmedico.registrar.php
@@ -1,14 +1,18 @@
 <?php
-$to      = 'luisvalve_1997@hotmail.com';
-$subject = 'the subject';
-$message = 'hello';
-$headers = 'From: webmaster@example.com' . "\r\n" .
-    'Reply-To: webmaster@example.com' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
-
-if(mail($to, $subject, $message, $headers)){
-    echo json_encode($resultado,true);
-}else{
-    echo 'error';
+try{
+    $to      = 'luisvalve_1997@hotmail.com';
+    $subject = 'the subject';
+    $message = 'hello';
+    $headers = 'From: webmaster@example.com' . "\r\n" .
+        'Reply-To: webmaster@example.com' . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+    
+    if(mail($to, $subject, $message, $headers)){
+        echo json_encode($resultado,true);
+    }else{
+        echo 'error';
+    }
+}catch(Exception $exc){
+    Funciones::mensaje($exc->getMessage(), "e");
 }
 ?>
