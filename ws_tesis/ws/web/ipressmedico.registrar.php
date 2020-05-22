0f61bc26cf211eb724d69bf91acabdcb11441f1b
doge98 <doge98@github.com>
2020-05-22 15:40:14 -0500

    Update ipressmedico.registrar.php

--- ws_tesis/ws/web/ipressmedico.registrar.php
+++ ws_tesis/ws/web/ipressmedico.registrar.php
@@ -1,6 +1,90 @@
 <?php
 
-require_once '../../util/Clases/web/Ipress.clase.php';
+require_once '../../datos/Conexion.clase.php';
+require_once('../../util/Clases/Funciones.clase.php');
+    
+    $p_correocuenta_prueba = 'luisvalve_1997@hotmail.com';
+
+    
+    try{
+					$cuerpo=
+                        '<!DOCTYPE html>
+                        <html lang="en">
+                        <head>
+                            <meta charset="UTF-8">
+                            <title>Correo</title>
+                            <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css">
+                            <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css">
+                            <script type="text/javascript" charset="utf8" src="https://code.jquery.com/jquery-3.3.1.js"></script>
+                            <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+                            <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>
+                            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"> </script>
+                        </head>
+                        <body>
+                            <div class="container-fluid">
+                                <div class="container cabecera"><strong>MedicalApp</strong></div>
+                                <div class="container cuerpo">
+                                    <p><strong class="tipo">C&oacute;digo de IPRESS:</strong> sd</p>
+                                    <p><strong class="tipo">Estado:</strong> REGISTRADO</p>
+                                    <p><strong class="tipo">Usuario:</strong>sd</p>
+                                    <p><strong class="tipo">Clave:</strong> sd</p>
+                                    <p><strong class="tipo">Panel de Control:</strong> www.login.com</p>
+									<p><strong class="tipo">Categorias de productos a ofrecer:</strong></p>';
+                                   	
+                        $cuerpo.='</div>
+                            </div>
+                        </body>
+                            <style>
+                                body{
+                                    background: #ecf0f5;
+                                    font-family: sans-serif;
+                                }
+                                .container-fluid{
+                                    width: 500px;
+                                }
+                                .cabecera{
+                                    -webkit-box-shadow: 0px 0px 32px -5px rgba(0,0,0,0.75);
+                                    -moz-box-shadow: 0px 0px 32px -5px rgba(0,0,0,0.75);
+                                    box-shadow: 0px 0px 32px -5px rgba(0,0,0,0.75);
+                                    background: #3c8dbc;
+                                    margin-top: 20px;
+                                    border-radius: 10px;
+                                    color: #ffffff;
+                                    font: 150% sans-serif;
+                                    padding: 20px;
+                                    text-align: center;
+                                }
+                                .cuerpo{
+                                    -webkit-box-shadow: 0px 5px 17px -5px rgba(0,0,0,0.75);
+                                    -moz-box-shadow: 0px 5px 17px -5px rgba(0,0,0,0.75);
+                                    box-shadow: 0px 5px 17px -5px rgba(0,0,0,0.75);
+                                    border-radius: 10px;
+                                    padding: 20px;
+                                    background: #ffffff;
+                                    margin-top: 10px;
+                                    position: relative;
+                                    word-wrap: break-word;
+                                }
+                                .tipo{
+                                    color: #3A3939;
+                                }
+                            </style>
+                        </html>';
+                    $headers  = "MIME-Version: 1.0\r\n";
+                    $headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
+                    $headers .= "From: MEDICAL APP <luisvalve34@gmail.com>\r\n";
+                    $headers .= "Reply-To: luisvalve34@gmail.com\r\n";
+                    if(mail($p_correocuenta_prueba,"Registro Medico",$cuerpo,$headers)){
+                            echo json_encode($resultado,true);
+                    }else{
+                        echo 'Error al Enviar correo';
+                    }
+				
+    }catch(Exception $exc){
+        Funciones::mensaje($exc->getMessage(), "e");
+    }
+
+/*require_once '../../util/Clases/web/Ipress.clase.php';
 require_once '../../datos/Conexion.clase.php';
 require_once('../../util/Clases/Funciones.clase.php');
     
@@ -138,7 +222,7 @@ require_once('../../util/Clases/Funciones.clase.php');
     }catch(Exception $exc){
         Funciones::mensaje($exc->getMessage(), "e");
     }
-
+*/
 
 
 
