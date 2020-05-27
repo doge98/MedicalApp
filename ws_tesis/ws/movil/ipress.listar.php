ed9f4d971588c1c1ddba617ffbbe76ca2c756b9c
doge98 <doge98@github.com>
2020-05-27 15:43:44 -0500

    Update ipress.listar.php

--- ws_tesis/ws/movil/ipress.listar.php
+++ ws_tesis/ws/movil/ipress.listar.php
@@ -1,14 +1,50 @@
 <?php
 
-require_once '../../util/Clases/movil/Ipress.clase.php';
-require_once '../../datos/Conexion.clase.php';
-require_once('../../util/Clases/Funciones.clase.php');
-    
-    try{
-        $objIpress=new Ipress();
-        $resultado=$objIpress->ipresslistar();
-        echo json_encode($resultado);
-    }catch(Exception $exc){
-        Funciones::mensaje($exc->getMessage(), "e");
+
+$prueba = '[
+    {
+        "codigo_unico_ipress":"00000001",
+        "nombre_comercial":"HOSPITAL IQUITOS ",
+        "latitud":"-3.762818",
+        "longitud":"-73.253653"
+    },
+    {
+        "codigo_unico_ipress":"00000023",
+        "nombre_comercial":"C.S. I-3 9 DE OCTUBRE",
+        "latitud":"-3.76457022",
+        "longitud":"-73.24459961"
+    },
+    {
+        "codigo_unico_ipress":"00000003",
+        "nombre_comercial":"HOSPITAL REGIONAL DE LORETO ",
+        "latitud":"-3.72696048",
+        "longitud":"-73.25341698"
+    },
+    {
+        "codigo_unico_ipress":"00000033",
+        "nombre_comercial":"ASOCIACION DE COMUNIDAD LOCAL DE ADMINISTRACION DE SALUD ",
+        "latitud":"-4.04314539",
+        "longitud":"-73.43853954"
+    },
+    {
+        "codigo_unico_ipress":"00000051",
+        "nombre_comercial":"6 DE OCTUBRE",
+        "latitud":"-3.76457022",
+        "longitud":"-73.24459961"
+    },
+    {
+        "codigo_unico_ipress":"00000052",
+        "nombre_comercial":"Route 1",
+        "latitud":"-7.891037",
+        "longitud":"-79.222803"
+    },
+    {
+        "codigo_unico_ipress":"00000053",
+        "nombre_comercial":"Route 2",
+        "latitud":"-7.892303",
+        "longitud":"-79.222445"
     }
+]';
+
+echo $prueba;
 ?>
