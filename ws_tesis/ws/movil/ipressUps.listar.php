9020e2470804ade841957c9dd1cd036e49d4c591
doge98 <doge98@github.com>
2020-05-31 14:13:11 -0500

    Update ipressUps.listar.php

--- ws_tesis/ws/movil/ipressUps.listar.php
+++ ws_tesis/ws/movil/ipressUps.listar.php
@@ -18,8 +18,8 @@ require_once('../../util/Clases/Funciones.clase.php');
                 $objIpress=new Ipress();
                 $objIpress->setP_codigounicoipress($p_codigounicoipress);
 	            $resultado=$objIpress->ipressupslistar();
-                Funciones::imprimeJSON(200, "Éxito", $resultado);
-                //echo json_encode($resultado)
+                //Funciones::imprimeJSON(200, "Éxito", $resultado);
+                echo $p_codigounicoipress;
             //}
     }catch(Exception $exc){
         Funciones::mensaje($exc->getMessage(), "e");
