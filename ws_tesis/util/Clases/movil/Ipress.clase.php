5c3b4dd2fbc0ec460d170b72a8fb29c882944642
doge98 <doge98@github.com>
2020-05-31 13:59:43 -0500

    sd

--- ws_tesis/util/Clases/movil/Ipress.clase.php
+++ ws_tesis/util/Clases/movil/Ipress.clase.php
@@ -26,19 +26,19 @@ class Ipress extends Conexion{
     }
 
     public function ipressupslistar(){
-        try {
-            $sql = "select * from ipress_ups iu 
+        try { 
+            $sql="select * from ipress_ups iu 
             inner join ups_especialidad ue on ue.codigo_upss_especialidad = iu.codigo_upss_especialidad
             inner join ups_general ug on ug.codigo_upss_general=ue.codigo_upss_general
             where iu.estado_movil='1' and iu.codigo_unico_ipress=:p_codigounicoipress
             order by ug.descripcion";
             $sentencia = $this->dblink->prepare($sql);
-            $sentencia->bindValue(":p_codigounicoipress",$this->getP_codigounicoipress());
+            $sentencia -> bindParam(":p_codigounicoipress", $this->getP_codigounicoipress());
             $sentencia->execute();
-            $resultado = $sentencia->fetchAll();
-            return $resultado;
+            $resultado = $sentencia->fetchAll(PDO::FETCH_ASSOC);
+            return $resultado;    
         } catch (Exception $exc) {
-            throw $exc;
+            throw  $ex;
         }
     }
 }
--- ws_tesis/ws/movil/ipressUps.listar.php
+++ ws_tesis/ws/movil/ipressUps.listar.php
@@ -1,27 +1,25 @@
 <?php
 
-require_once '../../util/Clases/web/Ipress.clase.php';
-require_once '../../datos/Conexion.clase.php';
+require_once('../../util/Clases/movil/Ipress.clase.php');
+require_once('../../datos/Conexion.clase.php');
 require_once('../../util/Clases/Funciones.clase.php');
     
-    //session_start();
-    //error_reporting(0);
     //$p_token = $_SESSION['token'];
-    //$p_codigounicoipress = $_SESSION["codigo_ipress"];
     $p_codigounicoipress = $_POST["p_codigounicoipress"];
 
-
     /*if(!isset($_SESSION["token"])){
-        Funciones::imprimeJSON(500,"Falta el token","");	
+        Funciones::imprimeJSON(500,"Falta el token","");
         exit();
     }*/
+    
     try{
         //require_once '../token.validar.php';
             //if(validarToken($p_token)){
                 $objIpress=new Ipress();
                 $objIpress->setP_codigounicoipress($p_codigounicoipress);
 	            $resultado=$objIpress->ipressupslistar();
-				echo resultado
+                //Funciones::imprimeJSON(200, "Éxito", json_encode($resultado));
+                Funciones::imprimeJSON(200, "Éxito", $resultado);
             //}
     }catch(Exception $exc){
         Funciones::mensaje($exc->getMessage(), "e");
