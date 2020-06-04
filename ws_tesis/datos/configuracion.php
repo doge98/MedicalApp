fcdc1fa45229f07a68886e8e461de5d84ab6e8bd
doge98 <doge98@github.com>
2020-06-04 02:22:52 -0500

    Update configuracion.php

--- ws_tesis/datos/configuracion.php
+++ ws_tesis/datos/configuracion.php
@@ -1,12 +1,7 @@
-	<?php
-
-
-//define("SERVIDOR_BD", "34.67.39.231");34.68.222.10
+<?php
 define("SERVIDOR_BD", "db-postgresql-nyc1-82778-do-user-7516899-0.a.db.ondigitalocean.com");
-//define("SERVIDOR_BD", "34.67.39.231");
 define("USUARIO_BD", "doadmin");
 define("CLAVE_BD", "p4nb6s9crfv118b7");
 define("PUERTO_BD", "25060");
 define("NOMBRE_BD", "tesis");
 error_reporting(E_ALL ^ E_NOTICE);
- 
